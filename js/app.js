038343528a711a8e7fed0128dcf64a3791ad940d
decentbi <54948169+decentbi@users.noreply.github.com>
2026-04-08 06:42:44 +0200

    fix

--- js/app.js
+++ js/app.js
@@ -1157,6 +1157,7 @@
       instagram: '<i class="fab fa-instagram"></i>',
       threads: '<i class="fab fa-threads"></i>',
       linkedin: '<i class="fab fa-linkedin-in"></i>',
+      facebook: '<i class="fab fa-facebook"></i>',
     };
     a.innerHTML = icons[type] || '<i class="fas fa-link"></i>';
     return a;
